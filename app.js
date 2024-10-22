299990dc03c5e842b53ea065567f728869643056
Jottazp <contato.rodrigues5000@gmail.com>
2024-10-22 20:05:11 -0300

    'adicionarLivro'

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ function renderList(doc){
     let li = document.createElement('li')
     let autor = document.createElement('span')
     let titulo = document.createElement('span')
+    let excluir = document.createElement('div');
 
+    li.setAttribute('data-id', doc.titulo);
     autor.textContent = doc.autor
     titulo.textContent = doc.titulo
 
@@ -36,4 +38,12 @@ db.collection('libri-collection')
     form.addEventListener('submit', (event)=>{
         event.preventDefault();
         alert('Formulário funcionando!')
+        db.collection('libri-collection').add({
+            autor: form.autor.value,
+            titulo: form.titulo.value
+        }).then(()=>{
+            form.autor.value = '';
+            form.titulo.value = '';
+            window.location.reload();
+        });
     });
